fc3fbbff03ba707903528df88f8e1e81fee179e8
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2022-09-08 13:22:47 +0200

    moved all to HQgeneric

---
+++ uniform-hqSchema/src/HQgeneric/FourteenPoints.hs
@@ -0,0 +1,92 @@
+ -----------------------------------------------------------------------------
+--
+-- Module      :  process the fourteen point example data set 
+-- as a comprehensive example 
+
+--  used HQfaces_test as example 
+
+-----------------------------------------------------------------------------
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE OverloadedStrings
+    , RecordWildCards
+    , UndecidableInstances     #-}
+
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-# OPTIONS_GHC -w #-}
+{-# HLINT ignore "Avoid lambda" #-}
+{-# HLINT ignore "Use map once" #-}
+{-# HLINT ignore "Redundant $" #-}
+
+module HQgeneric.FourteenPoints
+    where
+
+import           Test.Framework hiding (scale, (.&.))
+
+import UniformBase  
+-- import HQschema.HQexampleShort
+-- import HQschema.HQschemaShort
+-- import HQschema.HQfaces_test
+-- import HQschema.HQconstructions4graphics 
+-- -- import Control.Exception
+import Uniform.GeometryFunctions
+import Uniform.Point2d ()
+import ExampleData.Point2d
+import Uniform.TesselationHalfQuads
+import Uniform.TripleStore  
+import Uniform.Drawings
+  
+-- import Uniform.TripleRels
+-- import Data.List.Extra
+-- import Uniform.Drawings
+import Control.Monad.State  
+import Data.Functor.Identity
+-- import HQschema.HQconstructionsEdges
+import HQgeneric.HQconstructionsFaces
+import HQgeneric.HQconstructions4graphics
+import HQgeneric.HQschemaTop
+import Country.Schema
+import Country.Store 
+import Control.Monad.RWS (MonadWriter(tell))
+
+-- tess14 = delaunay2 fourteenPnt2d 
+-- -- extract the data into the half quad structure from the hgeometry processing
+-- trip14 = hqToTrip 600 . toHq1 $ tess14 
+-- -- produce the cat for the delaunay triangulation as extracted from hgeometry
+-- cat60 = catStoreEmpty 
+-- cat61 = catStoreBatch (map wrapIns . getAllTrips $ trip14) cat60
+cat61 = makeCatFrom  600 fourteenPnt2d
+-- add the rest of the hq structure to have the primal (delaunay)
+--  and the dual (voronoi) structure 
+
+-- process to construct the addins in hqfaces  -- need cleaning
+addtoCat :: [(CountryElement)] -> CatCountry -> CatCountry
+addtoCat ts cat0 = storeBatch (map wrapIns ts) cat0
+
+cat62 = addtoCat  (allAddins cat61) cat61
+
+-- the delaunay and voronoi diagram graphics
+
+hqV6 :: [(ObjCountry, (V2D, V2D))]
+hqV6 = evalTrans4query2cat pointsPairsv2 hqVoro cat62 
+hqD6 :: [(ObjCountry, (V2D, V2D))]
+hqD6 = evalTrans4query2cat pointsPairsv2 hqDela cat62 
+fig6voro :: [Figure V2D]
+fig6voro = map (\p -> figLine (dark green) p) . map snd $ hqV6
+fig6dela :: [Figure V2D]
+fig6dela = map (\p -> figLine (dark red) p) . map snd $ hqD6
+
+pageHQfourteenPoints :: ErrIO ()
+pageHQfourteenPoints = do 
+    putIOwords ["the production of the catStore for fourteen points example"]
+    putIOwords ["the points", showAsLines fourteenPnt2d]
+    putIOwords ["the voronoi lines  \n ", showT $ hqV6]
+    putIOwords ["the delaunay lines  \n ", showT $ hqD6]
+
+    showFacePage2 (fig6voro ++ fig6dela)
---
+++ uniform-hqSchema/src/HQgeneric/HQschemaTop.hs
@@ -0,0 +1,116 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  HQschema Top
+--      the operations applicable to the HQschema as necessary
+--      to build the catStore 
+
+-----------------------------------------------------------------------------
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE DoAndIfThenElse       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PackageImports        #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE DeriveFunctor  #-}
+
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+{-# HLINT ignore "Redundant $" #-}
+
+-- {-# OPTIONS_GHC  -fno-warn-warnings-deprecations #-}
+
+
+module HQgeneric.HQschemaTop 
+    -- (module HQschema.HQschemaTop
+    -- , module Uniform.TripleStore
+    -- , StoreTessShortElement (..)
+    -- , CatStoreTessShort (..)
+    -- , CatStoreState
+    -- )
+where
+
+import Uniform.Point2d
+import Uniform.TripleStore
+-- import Uniform.Point2d
+import UniformBase
+import Control.Monad.State
+import Country.Schema
+import Country.Store 
+-- import HQschema.HQschemaShort
+import Uniform.TesselationHalfQuads
+import Uniform.GeometryFunctions
+-- import Uniform.TripleStore
+import HQgeneric.HQconstructions4graphics 
+import HQgeneric.HQconstructionsFaces
+import HQgeneric.HQconstructionsEdges
+
+
+
+-- makeTripNode :: Int -> NodeHQ -> StoreTessShortElement
+-- -- | convert trip_xy   hqnx,   
+-- -- a is NodeID or FaceID (for center )
+-- -- note: the Face is the dual of the Node 
+-- the first field is just the id to check
+makeTripNode  i (NodeHQ j pnt) = reorg214  ( Node i, XY
+                    , PointTag  (Pnt2d (_p2id  pnt) (_v2 pnt)))
+
+-- getAllTrips :: TessShortHQtriples -> [StoreTessShortElement]
+getAllTrips hqt = concat [_NodesTrip hqt, _FacesTrip hqt, _HQtrips hqt]
+
+-- makeTripHq :: Int -> Int -> HQdataHQ -> [StoreTessShortElement]
+-- convert the HQ data to StoreTessShortElements
+makeTripHq offset i hq = catMaybes [hqnode,  hqface, hqtwin, hqhalflength]
+    where
+        hqid =   HalfQuad$ i 
+        -- hqnode, hqface, hqtwin, hqhalflength -- :: Maybe StoreTessShortElement
+        hqnode = Just . reorg214  $ (hqid, HqNode,  Node   . (+offset) . hq_node $ hq)
+        hqface =   fmap  (\fi -> reorg214 (hqid, HqFace,   Face  . (+offset) $ fi)) (Just . hq_face  $ hq)
+        -- in qhull is outerface = Nothing
+        -- in hgeometry is outerface just one of the faces
+        hqnext =  (hqid, NextHq,   HalfQuad  . (+offset) . hq_orbitNext $   hq)        
+        hqtwin = Just . reorg214 $ (hqid, Twin, HalfQuad  . (+offset) . hq_twin $ hq)
+        hqhalflength = Nothing 
+            -- Just $ (hqid, Dist, LengthTag . Length . halflength $ hq) 
+
+
+-- hqToTrip :: Int -> TesselationHQ ->  TessShortHQtriples
+hqToTrip offset teshq  = HQtriples
+    { _NodesTrip = zipWith (makeTripNode) [offset ..] (_Nodes teshq) 
+    -- , _FacesTrip = zipWith (makeTripFace) [offset ..] (_Faces teshq)
+    -- are constructed later 
+    , _FacesTrip = []
+    , _HQtrips   = concat $ zipWith (makeTripHq offset)   [offset ..] (_HQdatas teshq)
+    } 
+
+
+-- cat400 :: CatStoreTessShort
+-- cat400 = catStoreEmpty
+-- cat401 :: CatStore ObjPoint MorphTessShort
+-- intoCat :: [(StoreTessShortElement)] -> CatStoreTessShort
+intoCat ts = storeBatch (map wrapIns ts) $ storeEmpty -- cat400
+
+
+
+-- makeCat :: 
+
+makeCatFrom offset pnts = intoCat . getAllTrips . hqToTrip offset . toHq1 . delaunay2 $ pnts
+
+
+forEdges2points = [lengthHQtriple, midpointHQtriple]  
+forFaces = [circumcenter2triple, incenter2triple]
+forFaces2 = [area2triples]
+-- forqueries = [points12]
+-- theCats = [tess41short, tess51short]
+-- additinsPoints :: CatStoreTessShort -> [StoreTessShortElement]
+additinsPoints cat =  concat [evalTrans4query2cat trans points12 cat | trans <-[lengthHQtriple, midpointHQtriple]  ] -- trans query cat
+additinsAreas cat  =  concat [evalTrans4query2cat trans coords2faces cat | trans <-[area2triples] ] -- trans query cat
+additinsCenters cat =  catMaybes . concat   $ [evalTrans4query2cat trans coords2faces cat | trans <-[circumcenter2triple, incenter2triple]  ] -- trans query cat
+allAddins cat = concat [additinsPoints cat, additinsAreas cat, additinsCenters cat]
